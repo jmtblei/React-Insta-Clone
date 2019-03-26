ada4312f3471027c3a7b842f14cc99b7c4fba62b
jmtblei <47536941+jmtblei@users.noreply.github.com>
2019-03-26 16:05:21 -0700

    search filter and like counter functional, add comment still only half functional. styling needed

---
+++ dummyinstagram/src/components/PostContainer/LikesContainer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const LikesContainer = props => {
+    return (
+        <div onClick={props.upLike}>         
+            <div className="post-stats">
+                <div className="post-icons">
+                    <i className="far fa-heart"></i>
+                </div>   
+                <div className="post-icons">
+                    <i className="far fa-comment"></i>
+                </div>
+            </div>
+            <div className="likes weighted indented">
+                {props.likes} Likes
+            </div> 
+        </div>
+    )
+}
+
+export default LikesContainer;
--- dummyinstagram/src/components/PostContainer/Post.js
+++ dummyinstagram/src/components/PostContainer/Post.js
@@ -1,50 +1,66 @@
 import React from 'react';
 import './Post.css';
 import CommentSection from '../CommentSection/CommentSection';
+import LikesContainer from './LikesContainer';
+import PropTypes from 'prop-types';
 
-const Post = props => {
-    return (
+class Post extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        likes: props.post.likes
+      };
+    }
+    upLike = () => {
+      let likes = this.state.likes + 1;
+      this.setState({ likes });
+    };
+    render() {
+      return (
         <div className="top-border">
             <div className="post-container">
                 <div className="thumbnail-username">
                     <div className="thumbnail">
                             <img 
-                            src={props.post.thumbnailUrl}
+                            src={this.props.post.thumbnailUrl}
                             alt=""
                             />
                     </div>
                     <div className="weighted indented">
                         <p>
-                            {props.post.username}                      
+                            {this.props.post.username}                      
                         </p>
                     </div>
                 </div>
                 <div>
                     <img 
-                    src={props.post.imageUrl}
+                    src={this.props.post.imageUrl}
                     alt=""
                     />
                 </div>
-                <div className="post-stats">
-                    <div className="post-icons">
-                        <i className="far fa-heart"></i>
-                    </div>   
-                    <div className="post-icons">
-                        <i className="far fa-comment"></i>
-                    </div>
-                </div>
-                <div className="likes weighted indented">
-                    {props.post.likes} Likes
-                </div>
+                    <LikesContainer 
+                    upLike={this.upLike}
+                    likes={this.state.likes}
+                    />
                 <div>
-                    <CommentSection comments={props.post.comments}/>
+                    <CommentSection 
+                    comments={this.props.post.comments}
+                    />
                 </div>
                 <div className="faded">
-                    {props.post.timestamp}
+                    {this.props.post.timestamp}
                 </div>
             </div>
         </div>
-    )
+      )}
 }
+
+Post.propTypes = {
+    post: PropTypes.shape({
+      username: PropTypes.string,
+      thumbnailUrl: PropTypes.string,
+      imageUrl: PropTypes.string
+    })
+  };
 
 export default Post;
